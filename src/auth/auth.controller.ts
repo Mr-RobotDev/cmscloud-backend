55f9163f58162f304679fc4edad4e8686d599846
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-03-19 20:27:24 +0500

    feat: auth resource complete

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { Public } from '../common/decorators/public.decorator';
+import { SignUpDto } from './dto/signup.dto';
+
+@Public()
+@Controller({
+  path: 'auth',
+  version: '1',
+})
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() signUpDto: SignUpDto) {
+    return this.authService.signup(signUpDto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
+import { LoginDto } from './dto/login.dto';
+import { LoginSuccess } from './types/login-success.type';
+import { SignUpDto } from './dto/signup.dto';
+import { User } from './types/user.type';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signup(signUpDto: SignUpDto): Promise<{ user: User }> {
+    const user = await this.userService.getUserByEmail(signUpDto.email);
+    if (user) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const newUser = await this.userService.createUser(signUpDto);
+    return {
+      user: {
+        id: newUser._id,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    };
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginSuccess> {
+    const user = await this.userService.getUserByEmail(loginDto.email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid password');
+    }
+    const payload = {
+      sub: user._id,
+      email: user.email,
+      role: user.role,
+    };
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+        phone: user.phone,
+      },
+      token,
+    };
+  }
+}
